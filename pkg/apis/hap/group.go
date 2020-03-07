811dd1275d651f335503ccff2ba5f8010b47f23f
anirtek <atekade1@binghamton.edu>
2020-03-06 17:42:05 -0800

    Initial Commit

---
+++ pkg/apis/hap/group.go
@@ -0,0 +1,6 @@
+// Package hap contains hap API versions.
+//
+// This file ensures Go source parsers acknowledge the hap package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package hap
