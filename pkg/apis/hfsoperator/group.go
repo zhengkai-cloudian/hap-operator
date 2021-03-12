81033f85e5068941080b41826c0217d70e50b8e3
anirtek <atekade1@binghamton.edu>
2021-03-11 22:54:24 -0800

    Cleaning repo by deleting wrong API CRDs and Controllers  Changes to be committed: 	deleted:    deploy/crds/hfs-operator.cloudian.com_hapoperators_crd.yaml 	deleted:    deploy/crds/hfs-operator.cloudian.com_v1alpha1_hapoperator_cr.yaml 	deleted:    deploy/crds/hsc-operator.cloudian.com_hapoperators_crd.yaml 	deleted:    deploy/crds/hsc-operator.cloudian.com_v1alpha1_hapoperator_cr.yaml 	modified:   deploy/operator.yaml 	deleted:    pkg/apis/addtoscheme_hfsoperator_v1alpha1.go 	deleted:    pkg/apis/addtoscheme_hscoperator_v1alpha1.go 	deleted:    pkg/apis/hfsoperator/group.go 	deleted:    pkg/apis/hfsoperator/v1alpha1/doc.go 	deleted:    pkg/apis/hfsoperator/v1alpha1/hapoperator_types.go 	deleted:    pkg/apis/hfsoperator/v1alpha1/register.go 	deleted:    pkg/apis/hfsoperator/v1alpha1/zz_generated.deepcopy.go 	deleted:    pkg/apis/hscoperator/group.go 	deleted:    pkg/apis/hscoperator/v1alpha1/doc.go 	deleted:    pkg/apis/hscoperator/v1alpha1/hapoperator_types.go 	deleted:    pkg/apis/hscoperator/v1alpha1/register.go 	deleted:    pkg/apis/hscoperator/v1alpha1/zz_generated.deepcopy.go 	deleted:    pkg/controller/add_hapoperator.go 	deleted:    pkg/controller/hapoperator/hapoperator_controller.go

--- pkg/apis/hfsoperator/group.go
+++
@@ -1,6 +0,0 @@
-// Package hfsoperator contains hfsoperator API versions.
-//
-// This file ensures Go source parsers acknowledge the hfsoperator package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package hfsoperator
--- pkg/apis/hscoperator/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1alpha1 contains API Schema definitions for the hsc-operator v1alpha1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=hsc-operator.cloudian.com
-package v1alpha1
